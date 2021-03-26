e3dea9681fcaa85e3675b90ec26aaf57263bbd5f
prof. Martin Morães <martin.moraes@outlook.com>
2021-03-26 08:44:21 -0300

    Organizado a apresentação

--- src/Principal.java
+++ src/Principal.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		
 		/*
 		 * Biblioteca Gson
 		 * https://sites.google.com/site/gson/Home
@@ -20,28 +21,45 @@ public class Principal {
 		listaCliente.add(new Cliente("Carlos",23, new Endereco("uuuuu", "1", "UUUUUUU")));
 		listaCliente.add(new Cliente("Pedro", 24, new Endereco("iiiiii", "1", "IIIIIII")));
 		listaCliente.add(new Cliente("Vania", 25, new Endereco("oooooo", "1", "OOOOOOOOO")));
-		System.out.println("ArrayList: " + listaCliente.toString());
+//		System.out.println("ArrayList: " + listaCliente.toString());
 		
 		
-		//Server side
-		//Passando de ArrayList para Json(String)
+		 String stringJSON = "[{\"Nome\":\"João\",\"idade\":21,\"Endereco\":{\"rua\":\"xxxxxx\",\"numero\":\"1\",\"cidade\":\"XXXXXX\"}},{\"Nome\":\"Maria\",\"idade\":22,\"Endereco\":{\"rua\":\"yyyyyy\",\"numero\":\"1\",\"cidade\":\"YYYYYY\"}},{\"Nome\":\"Carlos\",\"idade\":23,\"Endereco\":{\"rua\":\"uuuuu\",\"numero\":\"1\",\"cidade\":\"UUUUUUU\"}},{\"Nome\":\"Pedro\",\"idade\":24,\"Endereco\":{\"rua\":\"iiiiii\",\"numero\":\"1\",\"cidade\":\"IIIIIII\"}},{\"Nome\":\"Vania\",\"idade\":25,\"Endereco\":{\"rua\":\"oooooo\",\"numero\":\"1\",\"cidade\":\"OOOOOOOOO\"}}]";
+		
+		Principal principal = new Principal();
 		Gson gson = new GsonBuilder().create();
-	    String arrayListToJson = gson.toJson(listaCliente);
-		System.out.println("String Json: " + arrayListToJson.toString());
+
+		principal.formListToJSON(gson, listaCliente);
+				
+		principal.fromArrayJsonToArray(gson, stringJSON);
 		
+		principal.fromArrayToList(gson, stringJSON, principal);
+	
+	}
+
+
+	private void fromArrayToList(Gson gson,  String stringJSON, Principal principal) {
+		Cliente[] arrayClientes = principal.fromArrayJsonToArray(gson, stringJSON);
 		
-		//client side
-		//Passando de Json(String) para Array do tipo "Cliente"
-		Cliente[] JsomEmArray = gson.fromJson(arrayListToJson, Cliente[].class);
+		List<Cliente> listCliente = Arrays.asList(arrayClientes);
 		
-		//Passando de Array para ArrayList do tipo "Cliente"
-		List<Cliente> minhaArrayListNOVAMENTE = new ArrayList<Cliente>();		
-		minhaArrayListNOVAMENTE = Arrays.asList(JsomEmArray);
+		listCliente.forEach( cliente -> {
+			System.out.println("Cada cliente: " + cliente);
+		});
 		
-		//Mostrando a nova ArrayList de "Cliente"
-		for(Cliente umCliente : minhaArrayListNOVAMENTE){
-			System.out.println("Cada cliente: " + umCliente.toString());
-		}
+	}
+
+	private Cliente[] fromArrayJsonToArray(Gson gson,  String stringJSON) {
+		Cliente[] arrayObjCliente = gson.fromJson(stringJSON, Cliente[].class);
+		
+		return arrayObjCliente;
+	}
+
+	private String formListToJSON(Gson gson, ArrayList<Cliente> listaCliente) {
+		String stringJson = gson.toJson(listaCliente);
+		System.out.println("String Json: " + stringJson.toString());
+		return stringJson;
+
 	}
 
 }
